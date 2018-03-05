ea9fc59d36601e1a1e8652949dc49dd7e179b648
HaoTse <top30339@gmail.com>
2018-03-05 21:20:35 +0800

    Completet reading data
    - Initial requirements.

--- trader.py
+++ trader.py
@@ -1,4 +1,22 @@
-# You can write code above the if-main block.
+import pandas as pd
+
+class Trader:
+
+    def train(self, dataset):
+        print(dataset.head())
+
+    def predict_action(self, row):
+        return '1'
+
+    def re_training(self, i):
+        print(i)
+
+def load_data(file_name):
+    titles = ['open', 'high', 'low', 'close']
+    df = pd.read_csv(file_name, delimiter=',', header=None)
+    df.columns = titles
+    
+    return df
 
 if __name__ == '__main__':
     # You should not modify this part.
@@ -16,6 +34,7 @@ if __name__ == '__main__':
                         help='output file name')
     args = parser.parse_args()
     
+    load_data(args.training)
     # The following part is an example.
     # You can modify it at will.
     training_data = load_data(args.training)
@@ -26,8 +45,8 @@ if __name__ == '__main__':
     with open(args.output, 'w') as output_file:
         for row in testing_data:
             # We will perform your action as the open price in the next day.
-            action = trader.predict_action(datum)
+            action = trader.predict_action(row)
             output_file.write(action)
 
             # this is your option, you can leave it empty.
-            trader.re_training(i)
+            # trader.re_training(i)
