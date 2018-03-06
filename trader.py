e073c29aff03c0665449ffe53ebbabc0e3a04e6d
HaoTse <top30339@gmail.com>
2018-03-06 15:47:08 +0800

    Add features selection

--- trader.py
+++ trader.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.feature_selection import f_regression
 
 class Trader:
     reg = linear_model.LinearRegression()
 
-    def train(self, dataset):
+    def __process(self, dataset):
         targets = dataset.loc[1:, 'open']
         features = dataset.iloc[:-1]
+
+        return targets, features
+
+    def train(self, dataset):
+        targets, features = self.__process(dataset)
 
         reg = self.reg
         reg.fit(features, targets)
@@ -14,11 +21,44 @@ class Trader:
         print("Socre Training: ", reg.score(features, targets))
 
     def test(self, dataset):
-        targets = dataset.loc[1:, 'open']
-        features = dataset.iloc[:-1]
+        targets, features = self.__process(dataset)
 
         reg = self.reg
         print("Score Testing: ", reg.score(features, targets))
+
+    def selection(self, train_data, test_data):
+        train_targets, train_features = self.__process(train_data)
+        test_targets, test_features = self.__process(test_data)
+        F_values, _ = f_regression(train_features, train_targets)
+
+        features_and_f_values = list(zip(train_data.columns, F_values))
+        features_and_f_values.sort(key=lambda x: x[1], reverse=True)
+
+        features_num_seq = range(1, len(train_data.columns)+1)
+        result_test_score = list()
+        result_train_score = list()
+        for num in features_num_seq:
+            selected_features = [
+                feature_and_f_value[0]
+                for feature_and_f_value in features_and_f_values[:num]
+            ]
+
+            train_selected_features = train_features.loc[:, selected_features]
+            test_selected_features = test_features.loc[:, selected_features]
+
+            reg = linear_model.LinearRegression()
+            reg.fit(train_selected_features, train_targets)
+
+            result_train_score.append(reg.score(train_selected_features, train_targets))
+            result_test_score.append(reg.score(test_selected_features, test_targets))
+        
+        plt.plot(features_num_seq, result_train_score, marker='o', label='train')
+        plt.plot(features_num_seq, result_test_score, marker='*', label='test')
+        plt.xticks(features_num_seq)
+        plt.legend()
+        plt.xlabel('Number of features used')
+        plt.ylabel('Score')
+        plt.show()
 
     def predict_action(self, row):
         return '1'
@@ -58,6 +98,10 @@ if __name__ == '__main__':
     
     testing_data = load_data(args.testing)
     trader.test(testing_data)
+
+    # feature selection
+    trader.selection(training_data, testing_data)
+
     with open(args.output, 'w') as output_file:
         for row in testing_data:
             # We will perform your action as the open price in the next day.
